ef94af253d7e135f0a3e5bd5c542161fc03adafa
Maria Gabriela Sándigo Muñoz <137342274+MGSandigoMunoz@users.noreply.github.com>
2024-04-16 18:04:07 -0500

    Projects: styles and construction of each one

--- My_Portfolio/src/components/Projects/projects.jsx
+++ My_Portfolio/src/components/Projects/projects.jsx
@@ -7,11 +7,15 @@ import styles from "./projects.module.css"
 export const Projects = () => {
   return (
     <section className={styles.container}>
-      <h2 className={styles.title}>Proyectos</h2>
-      <div className={styles.projects}>{
-      projects.map((project, id)=>{
-          return <ProjectCard key={id} project={project}/>
-        })}
+      <div className={styles.content}>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projects}>{
+          projects.map((project, id)=>{
+              return <ProjectCard key={id} project={project}/>
+            })}
+        </div>
+        <div className={styles.topBlur}/>
+        <div className={styles.bottonBlur}/>
       </div>
     </section>
   )
