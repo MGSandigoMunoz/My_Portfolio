7bf9d770e97fe6d043812bb38d5a6212f900c7f7
Maria Gabriela Sándigo Muñoz <137342274+MGSandigoMunoz@users.noreply.github.com>
2024-04-23 16:39:59 -0500

    Contact: module in progress

--- My_Portfolio/src/components/AboutMe/aboutMe.jsx
+++ My_Portfolio/src/components/AboutMe/aboutMe.jsx
@@ -1,19 +1,20 @@
-import { getImageUrl } from "../../utils"
-import styles from "./aboutMe.module.css"
+import { getImageUrl } from "../../utils";
+import styles from "./aboutMe.module.css";
 
 export const AboutMe = () => {
   return (
-    <section className={styles.container}> 
-    <div className={styles.content}>
-    {/* <h3 className={styles.title}>On construction</h3> */}
-    <p className={styles.description2}>On construction...</p>
-    {/* <p className={styles.description1}>FullStack developer</p> */}
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <p className={styles.description2}>On construction...</p>
+        <img
+          src={getImageUrl("Grulla_Construction.gif")}
+          alt="Grulla image"
+          className={styles.grullaImg}
+        />
 
-    <img src={getImageUrl("Grulla_Construction.gif")} alt="Grulla image" className={styles.grullaImg}/>
-
-    <div className={styles.topBlur}/>
-    <div className={styles.bottonBlur}/>
-    </div>
-</section>
-  )
-}
+        <div className={styles.topBlur} />
+        <div className={styles.bottonBlur} />
+      </div>
+    </section>
+  );
+};
--- My_Portfolio/src/components/Contact/contact.jsx
+++ My_Portfolio/src/components/Contact/contact.jsx
@@ -1,18 +1,29 @@
-
+import { getImageUrl } from "../../utils";
 import styles from "./contact.module.css"
+
 
 export const Contact = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h2 className={styles.title}>
-          Contacto
+        Contact
         </h2>
-        <ul className={styles.contacts}>
-          <li className={styles.contact}>Email: mgabrielasm0119@gmail.com</li>
-          <li className={styles.contact}>Github: https://github.com/MGSandigoMunoz</li>
-          <li className={styles.contact}>Linkedin: www.linkedin.com/in/maria-gabriela-sandigo-munoz</li>
-        </ul>
+        <ul className={styles.description1}>
+           <li className={styles.description1}>Email: mgabrielasm0119@gmail.com</li>
+           <li className={styles.description1}> 
+           <img 
+            src={getImageUrl('github.png')} 
+            alt={`GithubIcon`}
+            className={styles.image}
+            />
+           <a href={'https://github.com/MGSandigoMunoz'} target="bank">Github</a></li>
+           <li className={styles.description1}>Linkedin: www.linkedin.com/in/maria-gabriela-sandigo-munoz</li>
+         </ul>
+        
+
+        <div className={styles.topBlur} />
+        <div className={styles.bottonBlur} />
       </div>
     </section>
   )
